e2a31fb4c6fb594d8f044701412d2728e4a206b7
Marcelo Gornstein <marcelog@gmail.com>
2017-02-25 16:31:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# About
+This is the example code that goes with the article at [http://marcelog.github.io/articles/aws_lambda_start_stop_ec2_instance.html](http://marcelog.github.io/articles/aws_lambda_start_stop_ec2_instance.html).
+
+This Lambda code is used to start and stop EC2 instances.
+
+See the article mentioned above for the details.
+
+Cheers :)
+
+# Using it
+```
+$ npm install
+$ zip -r ec2_start_stop.zip *
+```
+
+Read the article for the details.
+
+```json
+{
+  "action": "start"            // Or "stop"
+  "instanceId": "i-xxxx"
+}
+```
+
+# License
+The source code is released under Apache 2 License.
+
+Check [LICENSE](https://github.com/marcelog/aws-lambda-start-stop-instances/blob/master/LICENSE) file for more information.
---
+++ index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+var Promise = require('promise');
+
+var actions = {
+  start: function (ec2, instanceId) {
+    return new Promise(function (resolve, reject) {
+      var params = {
+        InstanceIds: [instanceId],
+        DryRun: false
+      };
+      ec2.startInstances(params, function(err, data) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  },
+  stop: function (ec2, instanceId) {
+    return new Promise(function (resolve, reject) {
+      var params = {
+        InstanceIds: [instanceId],
+        DryRun: false
+      };
+      ec2.stopInstances(params, function(err, data) {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  }
+};
+
+function describe(ec2, instanceId) {
+  return new Promise(function (resolve, reject) {
+    var params = {
+      DryRun: false,
+      InstanceIds: [instanceId]
+    };
+    ec2.describeInstances(params, function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      var ip = data.Reservations[0].Instances[0].PublicIpAddress;
+      var desc = '';
+      data.Reservations[0].Instances[0].Tags.forEach(function (tag) {
+        desc = desc + ' ' + tag.Key + '=' + tag.Value + ' / ';
+      });
+      return resolve({ip: ip, description: desc});
+    });
+  });
+}
+
+exports.handler = function (event, context, callback) {
+  var p = actions[event.action];
+  if (!p) {
+    return callback('unknown action');
+  }
+  var from = 'StartOrStop';
+  var msgAction = event.action.toUpperCase() + ': ' + event.instanceId;
+  var ec2 = new AWS.EC2();
+  return describe(ec2, event.instanceId).then(function (data) {
+    msgAction = msgAction + ' - ' + data.ip + ' - ' + data.description;
+    console.log('[INFO]', 'Attempting', msgAction);
+  }).then(function () {
+    console.log('[INFO]', 'Done', msgAction);
+    return p(ec2, event.instanceId);
+  }).then(function () {
+    callback();
+  }).catch(function (err) {
+    console.log('[ERROR]', JSON.stringify(err));
+    return callback(err);
+  });
+};
